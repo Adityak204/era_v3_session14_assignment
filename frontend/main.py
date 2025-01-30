ce8fe24cc6cb5991e5bc3e1017a199229122d677
Aditya Singh <adityak204@gmail.com>
2025-01-31 01:16:35 +0530

    Comment out static files mounting in frontend main.py

--- frontend/main.py
+++ frontend/main.py
@@ -6,7 +6,7 @@ import httpx
 import os
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
